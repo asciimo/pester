c6fb334edfab36047af6a455f2e7d1c72e836b2e
Stephen Calnan <asciimo@gmail.com>
2015-05-19 10:01:03 -0700

    Python install adjustments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-RUN echo 'Beginning pester...'
 FROM phusion/baseimage:0.9.16
 
-RUN pip install requests
+RUN sudo apt-get update
+RUN sudo apt-get install -y python
+RUN sudo apt-get install -y python-requests
+
 RUN mkdir /etc/service/pester
 ADD pester.py /etc/service/pester/run
+RUN chmod u+x /etc/service/pester/run
 
 CMD ["/sbin/my_init"]
 
--- pester.py
+++ pester.py
@@ -3,13 +3,14 @@ import datetime
 import time
 import requests
 
-endpoint = 'http://127.0.0.1:3030/'
+endpoint = 'http://m.trulia.com/'
 timeoutSeconds = 60 
 cookieName = 'redleader'
 userAgent = 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.99 Mobile Safari/537.36'
 headers = {'user-agent' : userAgent}
 cookies = {}
 cookieFile = 'cookies.txt'
+logDir = '/var/log/pester/'
 
 def writeCookie(cookie):
   f = open(cookieFile, 'w')
@@ -36,7 +37,7 @@ def pester():
   
 while(True):
   result = pester()
-  f = open('/var/log/pester.log', 'a')
+  f = open(logDir + 'pester.log', 'a')
   timestamp = datetime.datetime.strftime(datetime.datetime.now(), "%c")
   f.write(timestamp + ' ' + result + '\n')
   f.close()
